ca43f3d7d8725e94cf98174cdd2af607198ba5f7
norMudag <antajia.mudag@gmail.com>
2026-04-27 16:45:46 +0800

    .

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -54,7 +54,7 @@ const About = () => {
             </h1>
 
             <p className="text-base sm:text-lg text-muted-foreground mt-6 max-w-[550px] leading-relaxed">
-              I'm a passionate developer
+              I'm a passionate developer and
             </p>
 
             <p className="text-base sm:text-lg text-muted-foreground mt-4 max-w-[550px] leading-relaxed">
